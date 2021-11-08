c3cc5888d1160db8e8d692509f4c514637cf7d26
Mljungho <mathias.ljungholm@gmail.com>
2021-11-08 18:57:47 +0100

    Adding unit test for witdrawal. Adding implementation for withdraw

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -5,4 +5,14 @@ describe Atm do
     it 'is expected to hold $1000 when instantiated' do
         expect(subject.funds).to eq 1000
     end
+    
+    it 'is expected to reduce funds on withdraw' do
+        subject.withdraw 50
+        expect(subject.funds).to eq 950
+    end
+    
+    it 'is expected to reduce withdraw' do
+        expect { subject.withdraw 50 }
+        .to change {subject.funds }.from(1000).to(950)
+    end
 end
